c79db720383b378a804d06bad262d84384c82f2a
EKRAM-MOHAMED <ekrammohamed2013@gmail.com>
2022-10-06 15:29:50 +0000

    edit frontend API

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: 'Udagram Microservice',
+  apiHost: 'http://a1247dd9784c64b1bae65f6397aa7906-809473091.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
